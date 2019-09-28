5bc24cafa6e69b727bd2953661b8939d23c3f241
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2019-09-28 15:18:59 +0200

    Update ssc task

--- ssc/eval-ssc-large-512-notrunctrain.sh
+++ ssc/eval-ssc-large-512-notrunctrain.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 PYTHONPATH=.. python3 run_classifier.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 PYTHONPATH=.. python3 ../examples/run_glue.py \
   --task_name ssc \
   --do_train \
   --do_eval \
   --do_lower_case \
   --data_dir ~/data-eval \
-  --bert_model bert-large-uncased \
+  --model_type bert \
+  --model_name_or_path bert-large-uncased \
   --max_seq_length 512 \
   --filter_long_sequences_train \
-  --train_batch_size 32 \
+  --per_gpu_train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 3.0 \
   --output_dir ~/eval/ssc-large-512-notrunctrain/
